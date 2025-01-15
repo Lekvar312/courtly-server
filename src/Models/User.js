8631e8f04ae5355bdda2644064de7d371e1f1f8a
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-01-15 15:22:49 +0200

    add userRoutes

--- src/Models/User.js
+++ src/Models/User.js
@@ -4,6 +4,6 @@ const userSchema = new mongoose.Schema ({
   name:{type:String, required: true},
   email:{type:String, required: true},
   password:{type:String, required: true},
-})
+},{timestamp: true})
 
 export default mongoose.model('User', userSchema)
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,55 @@ import User from '../Models/User.js'
 
 const router  = express.Router()
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find().select('-password'); 
+    res.status(200).json(users); 
+  } catch (error) {
+    res.status(500).json({ message: 'Помилка сервера', error: e.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const {id} = req.params
+  try{
+    const user = await User.findById(id)
+    if (!user) return res.status(404).json({message:"Користувача не знайдено"})
+    res.status(200).json(user)
+  }catch(e){
+    res.status(500).json({ message: 'Помилка сервера', error: e.message });
+  }
+})
+
+router.put('/:id', async(req, res) => {
+  const {id} = req.params
+  const {name, email } = req.body
+  try{
+    const user = await User.findByIdAndUpdate(
+      id,
+      {name, email},
+      {new: true, runValidators: true}
+    )
+    if (!user) return res.status(404).json({message:"Користувача не знайдено"})
+    res.status(200).json({message: "Дані користувача успішно оновлено", user})
+  }catch(e){
+    res.status(500).json({ message: 'Помилка сервера', error: e.message });
+  }
+})
+
+router.delete('/:id', async(req, res) => {
+  const {id} = req.params
+  try{
+
+    const user = await User.findByIdAndDelete(id)
+    if (!user) return res.status(404).json({message:"Користувача не знайдено"})
+    res.status(200).json({message:'Користувача успішно видалено'})  
+
+  }catch(e){
+    res.status(500).json({ message: 'Помилка сервера', error: e.message });
+  }
+})
+
 router.post('/register', async (req, res)=> {
   const {name,email,password} = req.body
   try{
@@ -10,7 +59,7 @@ router.post('/register', async (req, res)=> {
     await user.save()
     res.status(201).json({ message: 'Користувача створено успішно', user });
   }catch(e){
-    res.status(500).json({ message: 'Помилка сервера', error: error.message });
+    res.status(500).json({ message: 'Помилка сервера', error: e.message });
   }
 })
 
