40eaa2b3487daff36c268829115654ace8f155c8
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-01-17 14:56:44 +0200

    add booking routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import userRoutes from './userRoutes.js'
 import courtRoutes from './courtRoutes.js'
+import bookingRoutes from './bookingRoutes.js'
 const router = express.Router()
 
 router.get('/test', (req, res) => {
@@ -8,5 +9,6 @@ router.get('/test', (req, res) => {
 })
 router.use('/users', userRoutes);
 router.use('/courts', courtRoutes);
+router.use('/booking', bookingRoutes);
 
 export default router
