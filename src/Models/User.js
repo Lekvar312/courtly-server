1a421ff1be558ddcb3700756d4da727cca98a142
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-01-17 13:08:17 +0200

    update User model, add roles

--- src/Models/User.js
+++ src/Models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema ({
   name:{type:String, required: true},
   email:{type:String, required: true},
   password:{type:String, required: true},
+  role:{type:String, enum:["admin", "user"], default: "user"}
 },{timestamp: true})
 
 export default mongoose.model('User', userSchema)
