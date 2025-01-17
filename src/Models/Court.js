e08c64b0d37c647aa5d1d087ae09d65e565e0da5
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-01-17 13:05:29 +0200

    add Court model

---
+++ src/Models/Court.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const courtSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  adress: {type: String, required: true},
+  type: {type: String, enum:['football', 'tennis', 'basketball'], required: true},
+  price: {type: Number, required: true},
+  workingHours:{
+    startTime: {type: String, default:'08:00'},
+    endTime: {type: String, default:'23:00'}
+  },
+})
+
+export default mongoose.model('Court', courtSchema)
