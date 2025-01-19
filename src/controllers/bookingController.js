33cb7269e9e49056a4c39ca5f882f13c7001979c
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-01-19 19:38:55 +0200

    add booking controller

---
+++ src/controllers/bookingController.js
@@ -0,0 +1,60 @@
+import Booking from '../models/Booking.js'
+import Court from '../models/Court.js'
+import User from '../models/User.js'
+
+class bookingController {
+  async createBooking (req, res) {
+    const {courtId, userId, date, startTime, endTime} = req.body
+    try{
+      const court = await Court.findById(courtId)
+      if(!court) return res.status(404).json({ message: "Майданчик не знайдено" })
+      const user = await User.findById(userId)
+      if(!user) return res.status(404).json({ message: "Користувача не знайдено" })
+      const existingBooging = await Booking.findOne({ courtId, userId, date, startTime, endTime })
+      if(existingBooging) return res.status(400).json({ message: 'Цей час вже заброньовано' });
+      const booking = new Booking({ courtId, userId, date, startTime, endTime })
+      await booking.save()
+      res.status(201).json({ message:"Бронювання успішно створено", booking })
+    }catch(e){
+      res.status(500).json({ message: "Помилка при створенні бронювання", error: e.message})
+    }
+  }
+
+  async getBookings (req, res) {
+    try{
+      const bookings = await Booking.find()
+        .populate('courtId', 'name address')
+        .populate('userId', 'name email')
+      res.status(200).json(bookings)
+  
+    }catch(e){
+      res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
+    }
+  }
+
+  async getBookingById (req, res) {
+    const {id} = req.params
+    try{
+      const booking = await Booking.findById(id)
+        .populate('courtId', 'name address')
+        .populate('userId', 'name email')
+      if(!booking) res.status(404).json({ message: "Бронювання не знайдено" })
+      res.status(200).json(booking)
+    }catch(e){
+      res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
+    }
+  }
+  async deleteBooking (req, res) {
+    const {id} = req.params
+    try{
+      const booking = await Booking.findByIdAndDelete(id)
+      if(!booking) return res.status(404).json({ message: "Бронювання не знайдено" })
+      res.status(200).json({ message:"Бронювання успішно видалено" })
+  
+    }catch(e){
+      res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
+    }
+  }
+}
+
+export default new bookingController()
--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,62 +1,14 @@
 import express from 'express'
-import Booking from '../Models/Booking.js'
-import Court from '../models/Court.js'
-import User from '../models/User.js'
+import bookingController from '../controllers/bookingController.js'
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-  const {courtId, userId, date, startTime, endTime} = req.body
-  try{
-    const court = await Court.findById(courtId)
-    if(!court) return res.status(404).json({ message: "Майданчик не знайдено" })
-    const user = await User.findById(userId)
-    if(!user) return res.status(404).json({ message: "Користувача не знайдено" })
-    const existingBooging = await Booking.findOne({ courtId, userId, date, startTime, endTime })
-    if(existingBooging) return res.status(400).json({ message: 'Цей час вже заброньовано' });
-    const booking = new Booking({ courtId, userId, date, startTime, endTime })
-    await booking.save()
-    res.status(201).json({ message:"Бронювання успішно створено", booking })
-  }catch(e){
-    res.status(500).json({ message: "Помилка при створенні бронювання", error: e.message})
-  }
-})
+router.post('/', bookingController.createBooking)
 
-router.get("/", async (req, res) => {
-  try{
-    const bookings = await Booking.find()
-      .populate('courtId', 'name address')
-      .populate('userId', 'name email')
-    res.status(200).json(bookings)
+router.get("/", bookingController.getBookings)
 
-  }catch(e){
-    res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
-  }
-})
+router.get("/:id", bookingController.getBookingById)
 
-router.get("/:id", async (req, res) => {
-  const {id} = req.params
-  try{
-    const booking = await Booking.findById(id)
-      .populate('courtId', 'name address')
-      .populate('userId', 'name email')
-    if(!booking) res.status(404).json({ message: "Бронювання не знайдено" })
-    res.status(200).json(booking)
-  }catch(e){
-    res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
-  }
-})
-
-router.delete("/:id", async (req, res) => {
-  const {id} = req.params
-  try{
-    const booking = await Booking.findByIdAndDelete(id)
-    if(!booking) return res.status(404).json({ message: "Бронювання не знайдено" })
-    res.status(200).json({ message:"Бронювання успішно видалено" })
-
-  }catch(e){
-    res.status(500).json({ message: "Помилка при отриманні бронювання", error: e.message})
-  }
-})
+router.delete("/:id", bookingController.deleteBooking)
 
 export default router
