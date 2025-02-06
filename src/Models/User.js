a18233c3e4fdcb5c4e00cbea929373b36389a7d0
Lekvar <145697822+Lekvar312@users.noreply.github.com>
2025-02-06 21:04:59 +0200

    make email unique

--- src/Models/User.js
+++ src/Models/User.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema ({
   name:{type:String, required: true},
-  email:{type:String, required: true},
+  email:{type:String, unique: true, required: true},
   password:{type:String, required: true},
   role:{type:String, enum:["admin", "user"], default: "user"}
-},{timestamp: true})
+},{timestamps: true})
 
 export default mongoose.model('User', userSchema)
